73c6761e65d91fd718097fd225144411cac8b1db
godrsxt <rushabgshitre360@gmail.com>
2026-07-19 17:32:41 +0530

    Auto-commit: settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,85 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.7.2" apply false
+        id("com.android.library") version "8.7.2" apply false
+        id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+        id("org.jetbrains.kotlin.multiplatform") version "2.0.21" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+        id("com.google.devtools.ksp") version "2.0.21-1.0.14" apply false
+        id("kotlin-kapt") version "2.0.21" apply false
+        id("com.android.cmake") version "3.22.1" apply false
+        id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application" || requested.id.id == "com.android.library") {
+                useModule("com.android.tools.build:gradle:8.7.2")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android" || requested.id.id == "org.jetbrains.kotlin.multiplatform" || requested.id.id == "org.jetbrains.kotlin.plugin.compose" || requested.id.id == "org.jetbrains.kotlin.plugin.serialization") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+            }
+            if (requested.id.id == "com.google.devtools.ksp") {
+                useModule("com.google.devtools.ksp:symbol-processing-api:2.0.21-1.0.14")
+            }
+            if (requested.id.id == "kotlin-kapt") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+            }
+            if (requested.id.id == "com.android.cmake") {
+                useModule("com.android.tools.build:gradle:8.7.2")
+            }
+            if (requested.id.id == "com.google.dagger.hilt.android") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
+            }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+        maven { url = uri("https://jitpack.io") }
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "SceneEditor"
+
+include(":app")
+include(":core:engine")
+include(":core:common")
+include(":feature:viewport")
+include(":feature:ui")
+include(":feature:scene")
+include(":feature:timeline")
+include(":feature:import_export")
+include(":feature:modeling")
+include(":feature:rendering")
+
+configureEach {
+    val name = this.name
+    when (name) {
+        "app" -> projectDir.set(file("app"))
+        "core:engine" -> projectDir.set(file("core/engine"))
+        "core:common" -> projectDir.set(file("core/common"))
+        "feature:viewport" -> projectDir.set(file("feature/viewport"))
+        "feature:ui" -> projectDir.set(file("feature/ui"))
+        "feature:scene" -> projectDir.set(file("feature/scene"))
+        "feature:timeline" -> projectDir.set(file("feature/timeline"))
+        "feature:import_export" -> projectDir.set(file("feature/import_export"))
+        "feature:modeling" -> projectDir.set(file("feature/modeling"))
+        "feature:rendering" -> projectDir.set(file("feature/rendering"))
+    }
+}
